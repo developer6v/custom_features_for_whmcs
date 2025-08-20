24108bac80580d2a891608f9c75ff0d60c0a64ef
Otávio de Oliveira <otaviodeveloper6v@gmail.com>
2025-08-20 13:41:04 -0300

    customização

--- src/Config/assets.php
+++ src/Config/assets.php
@@ -4,5 +4,5 @@
 function assets() {
     $assets = '<link rel="stylesheet" type="text/css" href="' . __DIR__ . '/../../public/css/style.css' . '">';
     $assets .= '<script type="text/javascript" src="' . __DIR__ . '/../../public/js/script.js' . '"></script>';
-
+    return $assets;
 }
