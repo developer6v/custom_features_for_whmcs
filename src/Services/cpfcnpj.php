8319af3815b71a7b73dddc5f35d26c2f8ad3da5b
Otávio de Oliveira <otaviodeveloper6v@gmail.com>
2025-08-28 08:11:50 -0300

    sdf

--- src/Services/cpfcnpj.php
+++ src/Services/cpfcnpj.php
@@ -3,55 +3,51 @@ function cpfcnpj_script() {
     return <<<HTML
 <script>
 (function(){
-  function digits(s){ return (s||'').replace(/\\D/g,''); }
+  function digits(s){ return (s||'').replace(/\D/g,''); }
 
   function toggleCompanyRequired(isCnpj){
-    var \$company = jQuery('input[name="companyname"]');
-    if(!\$company.length) return;
-    if(isCnpj){
-      \$company.attr('required', 'required').attr('aria-required', 'true');
-    }else{
-      \$company.removeAttr('required').removeAttr('aria-required');
-    }
+    var $company = jQuery('input[name="companyname"]');
+    if(!$company.length) return;
+    if(isCnpj){ $company.attr('required','required').attr('aria-required','true'); }
+    else { $company.removeAttr('required').removeAttr('aria-required'); }
   }
 
-  function maskCpfCnpj(\$el){
-    var v = digits(\$el.val());
-    if(v.length > 14) v = v.slice(0,14);
-    if(v.length <= 11){
-      if(v.length > 9){
-        v = v.replace(/^(\\d{3})(\\d{3})(\\d{3})(\\d{0,2}).*$/, "\$1.\$2.\$3-\$4");
-      } else if(v.length > 6){
-        v = v.replace(/^(\\d{3})(\\d{3})(\\d{0,3}).*$/, "\$1.\$2.\$3");
-      } else if(v.length > 3){
-        v = v.replace(/^(\\d{3})(\\d{0,3}).*$/, "\$1.\$2");
-      }
+  function maskCpfCnpj($el){
+    var d = digits($el.val());
+    if(d.length > 14) d = d.slice(0,14);
+    var v = d;
+
+    if(d.length <= 11){
+      // CPF
+      $el.attr('maxlength','14');
+      if(d.length > 9)       v = d.replace(/^(\d{3})(\d{3})(\d{3})(\d{0,2}).*$/, "$1.$2.$3-$4");
+      else if(d.length > 6)  v = d.replace(/^(\d{3})(\d{3})(\d{0,3}).*$/,      "$1.$2.$3");
+      else if(d.length > 3)  v = d.replace(/^(\d{3})(\d{0,3}).*$/,             "$1.$2");
     } else {
-      if(v.length > 12){
-        v = v.replace(/^(\\d{2})(\\d{3})(\\d{3})(\\d{4})(\\d{0,2}).*$/, "\$1.\$2.\$3/\$4-\$5");
-      } else if(v.length > 8){
-        v = v.replace(/^(\\d{2})(\\d{3})(\\d{3})(\\d{0,4}).*$/, "\$1.\$2.\$3/\$4");
-      } else if(v.length > 5){
-        v = v.replace(/^(\\d{2})(\\d{3})(\\d{0,3}).*$/, "\$1.\$2.\$3");
-      } else if(v.length > 2){
-        v = v.replace(/^(\\d{2})(\\d{0,3}).*$/, "\$1.\$2");
-      }
+      // CNPJ
+      $el.attr('maxlength','18');
+      if(d.length > 12)      v = d.replace(/^(\d{2})(\d{3})(\d{3})(\d{4})(\d{0,2}).*$/, "$1.$2.$3/$4-$5");
+      else if(d.length > 8)  v = d.replace(/^(\d{2})(\d{3})(\d{3})(\d{0,4}).*$/,        "$1.$2.$3/$4");
+      else if(d.length > 5)  v = d.replace(/^(\d{2})(\d{3})(\d{0,3}).*$/,               "$1.$2.$3");
+      else if(d.length > 2)  v = d.replace(/^(\d{2})(\d{0,3}).*$/,                      "$1.$2");
     }
-    \$el.val(v);
-    toggleCompanyRequired(digits(v).length > 11);
+
+    $el.val(v);
+    toggleCompanyRequired(d.length > 11);
   }
 
   jQuery(function(){
-    var \$field = jQuery('#customfield1');
-    var \$field2 = jQuery('#cl_custom_field_1');
-    if(\$field.length){
-      maskCpfCnpj(\$field);
-      \$field.on('input', function(){ maskCpfCnpj(\$field); });
+    var $field  = jQuery('#customfield1');
+    var $field2 = jQuery('#cl_custom_field_1');
+
+    if($field.length){
+      maskCpfCnpj($field);
+      $field.on('input blur paste', function(){ maskCpfCnpj($field); });
     }
 
-    if(\$field2.length){
-      maskCpfCnpj(\$field2);
-      \$field.on('input', function(){ maskCpfCnpj(\$field2); });
+    if($field2.length){
+      maskCpfCnpj($field2);
+      $field2.on('input blur paste', function(){ maskCpfCnpj($field2); });
     }
   });
 })();
