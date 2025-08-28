8388d7a976af50177e51591ae5810938b5fdff45
Otávio de Oliveira <otaviodeveloper6v@gmail.com>
2025-08-28 06:14:28 -0300

    sdf

--- src/Services/enderecos.php
+++ src/Services/enderecos.php
@@ -3,50 +3,52 @@ function enderecos() {
     return <<<HTML
 <script>
 (function(){
-  var panels = document.querySelectorAll('.panel-body');
-  var src = (document.getElementById('phonenumber') ? document.getElementById('phonenumber').closest('.panel-body') : null) || panels[0] || document;
-  var dst = (document.getElementById('domaincontactphonenumber') ? document.getElementById('domaincontactphonenumber').closest('.panel-body') : null) || panels[1] || panels[0] || document;
+  function autofillDomainAddress() {
+    // seleciona o primeiro formulário (cliente)
+    var formCliente = document.querySelector("form[data-gtm-form-interact-id]"); 
+    // seleciona o segundo formulário (domínio)
+    var formDominio = document.querySelector(".panel-body:nth-of-type(2)"); 
 
-  function getValue(id) {
-    var el = src.querySelector('#'+id+', [name="'+id+'"]');
-    return el ? el.value : "";
-  }
+    if (!formCliente || !formDominio) return;
 
-  function setValue(id, value) {
-    var el = dst.querySelector('#'+id+', [name="'+id+'"]');
-    if (el) {
-      el.value = value;
-      if (el.tagName === 'SELECT' && typeof jQuery !== 'undefined') jQuery(el).trigger('change');
+    function getValue(form, selector) {
+      var el = form.querySelector(selector);
+      return el ? el.value : "";
     }
-  }
+    function setValue(form, selector, value) {
+      var el = form.querySelector(selector);
+      if (el) el.value = value;
+    }
 
-  function autofillDomainAddress(){
-    var firstName = getValue("firstname");
-    var lastName  = getValue("lastname");
-    var email     = getValue("email");
-    var phone     = getValue("phonenumber") || getValue("domaincontactphonenumber");
-    var address1  = getValue("address1");
-    var address2  = getValue("address2");
-    var city      = getValue("city");
-    var state     = getValue("state");
-    var postcode  = getValue("postcode");
-    var empresa   = getValue("companyname");
+    // coleta valores do formulário cliente
+    var firstName = getValue(formCliente, "#firstname");
+    var lastName  = getValue(formCliente, "#lastname");
+    var email     = getValue(formCliente, "#email");
+    var phone     = getValue(formCliente, "#phonenumber");
+    var address1  = getValue(formCliente, "#address1");
+    var city      = getValue(formCliente, "#city");
+    var state     = getValue(formCliente, "#state");
+    var postcode  = getValue(formCliente, "#postcode");
+    var empresa   = getValue(formCliente, "#companyname");
 
-    setValue("firstname", firstName);
-    setValue("lastname", lastName);
-    setValue("email", email);
-    setValue("domaincontactphonenumber", phone);
-    setValue("address1", address1);
-    setValue("address2", address2);
-    setValue("city", city);
-    setValue("state", state);
-    setValue("postcode", postcode);
-    setValue("companyname", empresa);
+    // aplica no formulário domínio
+    setValue(formDominio, "#firstname", firstName);
+    setValue(formDominio, "#lastname", lastName);
+    setValue(formDominio, "#email", email);
+    setValue(formDominio, "#domaincontactphonenumber", phone);
+    setValue(formDominio, "#address1", address1);
+    setValue(formDominio, "#city", city);
+    setValue(formDominio, "#state", state);
+    setValue(formDominio, "#postcode", postcode);
+    setValue(formDominio, "#companyname", empresa);
   }
 
   jQuery(function(){
     autofillDomainAddress();
-    jQuery(src).on('input change blur', 'input,select', function(){ autofillDomainAddress(); });
+
+    var sel = "#firstname, #lastname, #email, #phonenumber, #address1, #city, #state, #postcode, #companyname";
+    jQuery(document).on("input change blur", sel, autofillDomainAddress);
+
     setTimeout(autofillDomainAddress, 300);
     setTimeout(autofillDomainAddress, 1000);
     setTimeout(autofillDomainAddress, 2000);
