8814356bf14d7240535f858eb1185dc4e6d203e8
Otávio de Oliveira <otaviodeveloper6v@gmail.com>
2025-08-28 17:41:38 -0300

    sdf

--- src/Services/cepvalidator.php
+++ src/Services/cepvalidator.php
@@ -14,52 +14,38 @@ function cepvalidator_script() {
         var id="cep-validator-msg";
         var \$msg=jQuery("#"+id);
         if(!\$msg.length){
-            \$msg=jQuery('<span id="'+id+'" style="color:red;font-size:12px;display:block;margin-top:4px;"></span>');
-            \$cep.after(\$msg);
+        \$msg=jQuery('<span id="'+id+'" style="color:red;font-size:12px;display:block;margin-top:4px;"></span>');
+        \$cep.after(\$msg);
         }
         \$msg.text(msg||'');
     }
     function validateCep(){
-        console.log("chamou")
         var \$cep=byName('postcode');
         if(!\$cep.length){return;}
         var cep=onlyDigits(\$cep.val());
         if(cep.length!==8){
-            setDisabled(true);
-            showMsg('CEP inválido');
-            return;
+        setDisabled(true);
+        showMsg('CEP inválido');
+        return;
         }
         jQuery.getJSON('https://viacep.com.br/ws/'+cep+'/json/').done(function(d){
-            if(d && d.erro){
-                setDisabled(true);
-                showMsg('CEP inválido');
-            } else {
-                setDisabled(false);
-                showMsg('');
-            }
-        }).fail(function(){
+        if(d && d.erro){
             setDisabled(true);
             showMsg('CEP inválido');
+        } else {
+            setDisabled(false);
+            showMsg('');
+        }
+        }).fail(function(){
+        setDisabled(true);
+        showMsg('CEP inválido');
         });
     }
-
-    // Função para aplicar a máscara de CEP
-    function applyCepMask() {
-        var \$cep = byName('postcode');
-        if ($\cep.length) {
-            \$cep.mask('00000-000'); // Máscara de CEP no formato XXXXX-XXX
-        }
-    }
-
-    // Verifica se o campo de CEP existe, e se sim, aplica a máscara e validação
-    var checkExist = setInterval(function() {
-        if (jQuery("input[name='postcode']").length) {
-            clearInterval(checkExist); // Quando o elemento aparecer, pare de verificar
-            // Aplique a máscara de CEP e a validação
-            applyCepMask();
-            jQuery(document).on('change blur input','input[name="postcode"]',function(){validateCep();});
-        }
-    }, 100); // Verifique a cada 100 milissegundos
+    jQuery(function(){
+        setDisabled(true);
+        validateCep();
+        jQuery(document).on('change blur input','input[name="postcode"]',function(){validateCep();});
+    });
     })();
     </script>
     HTML;
