e3ec9e5ec8f416b30aeec4f6aa5f9f6b9e07bca9
Otávio de Oliveira <otaviodeveloper6v@gmail.com>
2025-08-29 02:01:16 -0300

    sdf

--- src/Services/hideFieldsCheckout.php
+++ src/Services/hideFieldsCheckout.php
@@ -3,24 +3,26 @@ function hidefields() {
     return <<<HTML
         <script type="text/javascript">
             function checkAndHideFields() {
-                // Verifica se os campos e labels existem
+                // Verifica o "form-group" do campo "Número"
                 var numberFieldParent = document.querySelector('.form-group #cl_custom_field_18');
+                // Verifica o "form-group" do campo "Como você nos encontrou?"
                 var howYouFoundFieldParent = document.querySelector('.form-group #cl_custom_field_24');
+                // Verifica o "form-group" do campo "Complemento"
                 var complementoFieldParent = document.querySelector('.form-group #cl_custom_field_19');
                 
                 // Se o "form-group" do campo "Número" existir, esconde ele
                 if (numberFieldParent) {
-                    numberFieldParent.style.display = 'none'; // Esconde o campo e o label
+                    numberFieldParent.closest('.form-group').style.display = 'none'; // Esconde o form-group inteiro
                 }
 
                 // Se o "form-group" do campo "Como você nos encontrou?" existir, esconde ele
                 if (howYouFoundFieldParent) {
-                    howYouFoundFieldParent.style.display = 'none'; // Esconde o campo e o label
+                    howYouFoundFieldParent.closest('.form-group').style.display = 'none'; // Esconde o form-group inteiro
                 }
 
                 // Se o "form-group" do campo "Complemento" existir, esconde ele
                 if (complementoFieldParent) {
-                    complementoFieldParent.style.display = 'none'; // Esconde o campo e o label
+                    complementoFieldParent.closest('.form-group').style.display = 'none'; // Esconde o form-group inteiro
                 }
 
                 // Se todos os campos forem encontrados, para a execução
