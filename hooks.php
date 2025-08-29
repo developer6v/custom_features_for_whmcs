e664975e140549a44a37fc0d394cec9165da6464
Otávio de Oliveira <otaviodeveloper6v@gmail.com>
2025-08-29 01:50:58 -0300

    sdf

--- hooks.php
+++ hooks.php
@@ -3,6 +3,7 @@ require_once __DIR__ . '/src/Services/cepvalidator.php';
 require_once __DIR__ . '/src/Services/cpfcnpj.php';
 require_once __DIR__ . '/src/Services/enderecos.php';
 require_once __DIR__ . '/src/Services/Domain/domain.php';
+require_once __DIR__ . '/src/Services/hideFieldsCheckout.php';
 
 if (!defined('WHMCS')) { die('Access denied'); }
 
@@ -35,20 +36,21 @@ add_hook('AfterRegistrarRegistrationFailed', 1, function($vars) {
 });
 
 
+
+// Remover Campos Checkout
+add_hook('ClientAreaFooterOutput', 1, function($vars) {
+    return hidefields();
+});
+
+
+
 function add_cep_mask_script($vars) {
     // Verifica se a página atual é a de checkout ou onde o campo CEP está presente
     if ($vars['filename'] == 'order' || $vars['filename'] == 'cart') {
         // Adiciona o script do jQuery Mask
         $maskScript = '
             <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script>
-            <script>
-            jQuery(document).ready(function($) {
-                // Aplica a máscara de CEP
-                $("input[name=\'postcode\']").mask("00000-000");
-            });
-            </script>
         ';
-        // Retorna o script para ser inserido na página
         return $maskScript;
     }
 }
---
+++ src/Services/hideFieldsCheckout.php
@@ -0,0 +1,37 @@
+<?php
+function hidefields() {
+    return <<<HTML
+        <script type="text/javascript">
+            // Esconde o campo "Número" e o label
+            var numberField = document.getElementById('cl_custom_field_18');
+            var numberLabel = document.querySelector('label[for="cl_custom_field_18"]');
+            if (numberField) {
+                numberField.style.display = 'none';
+                if (numberLabel) {
+                    numberLabel.style.display = 'none'; // Remove o label
+                }
+                numberField.value = 'N/A'; // Atribui "N/A" ao valor do campo
+            }
+
+            // Esconde o campo "Como você nos encontrou?" e o label
+            var howYouFoundField = document.getElementById('cl_custom_field_24');
+            var howYouFoundLabel = document.querySelector('label[for="cl_custom_field_24"]');
+            if (howYouFoundField) {
+                howYouFoundField.style.display = 'none';
+                if (howYouFoundLabel) {
+                    howYouFoundLabel.style.display = 'none'; // Remove o label
+                }
+            }
+
+            // Esconde o campo "Complemento" e o label
+            var complementoField = document.getElementById('cl_custom_field_19');
+            var complementoLabel = document.querySelector('label[for="cl_custom_field_19"]');
+            if (complementoField) {
+                complementoField.style.display = 'none';
+                if (complementoLabel) {
+                    complementoLabel.style.display = 'none'; // Remove o label
+                }
+            }
+        </script>
+        HTML;
+}
