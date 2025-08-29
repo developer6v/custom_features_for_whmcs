a6029c8ab2a6b2eb6eb7f12ad6f4563c61660d68
Otávio de Oliveira <otaviodeveloper6v@gmail.com>
2025-08-29 02:17:25 -0300

    sdf

--- hooks.php
+++ hooks.php
@@ -48,18 +48,22 @@ add_hook('ClientAreaFooterOutput', 1, function($vars) {
 
 
 
-
 function add_cep_mask_script($vars) {
     // Verifica se a página atual é a de checkout ou onde o campo CEP está presente
     if ($vars['filename'] == 'order' || $vars['filename'] == 'cart') {
         // Adiciona o script do jQuery Mask
         $maskScript = '
             <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script>
+            <script>
+            jQuery(document).ready(function($) {
+                // Aplica a máscara de CEP
+                $("input[name=\'postcode\']").mask("00000-000");
+            });
+            </script>
         ';
         return $maskScript;
     }
 }
-
 // Registra o hook para adicionar o script no cabeçalho
 add_hook('ClientAreaHeadOutput', 1, 'add_cep_mask_script');
 
