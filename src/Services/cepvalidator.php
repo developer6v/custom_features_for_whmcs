5c168837fe1d42a75aeaeabfabeb08eefe19d72c
Otávio de Oliveira <otaviodeveloper6v@gmail.com>
2025-09-06 03:54:52 -0300

    take it

--- src/Services/cepvalidator.php
+++ src/Services/cepvalidator.php
@@ -2,74 +2,76 @@
 function cepvalidator_script() {
     return <<<HTML
 <script>
-(function(){
-  'use strict';
-  alert('[cepvalidator] init');
+;(() => {
+  // --- Utils (vanilla) ---
+  const $ = (sel, root=document) => root.querySelector(sel);
+  const $$ = (sel, root=document) => root.querySelectorAll(sel);
+  const onlyDigits = s => (s||'').replace(/\\D/g, '');
+  const getSubmitButtons = () =>
+    Array.from($$('button[type="submit"], input[type="submit"], button#checkout, #place_order'));
+  const setDisabled = dis => getSubmitButtons().forEach(b => b.disabled = !!dis);
 
-  function onlyDigits(s){ return (s||'').replace(/\\D/g,''); }
-  function btns(){ return jQuery('button[type="submit"], input[type="submit"], button#checkout, #place_order'); }
-  function setDisabled(dis){ btns().prop('disabled', dis); }
-
-  function showMsg($cep, msg){
-    var id = 'cep-validator-msg';
-    var $msg = jQuery('#'+id);
-    if(!$msg.length){
-      $msg = jQuery('<span id="'+id+'" style="color:red;font-size:12px;display:block;margin-top:4px;"></span>');
-      $cep.after($msg);
+  const ensureMsgEl = (afterEl) => {
+    const id = 'cep-validator-msg';
+    let msg = document.getElementById(id);
+    if (!msg) {
+      msg = document.createElement('span');
+      msg.id = id;
+      msg.style.cssText = 'color:red;font-size:12px;display:block;margin-top:4px;';
+      if (afterEl && afterEl.parentNode) {
+        afterEl.parentNode.insertBefore(msg, afterEl.nextSibling);
+      } else {
+        document.body.appendChild(msg);
+      }
     }
-    $msg.text(msg || '');
-  }
+    return msg;
+  };
+  const showMsg = (input, text) => { ensureMsgEl(input).textContent = text || ''; };
 
-  function maskCep($cep){
-    $cep.on('input', function(){
-      var v = onlyDigits(this.value).slice(0,8);
-      if(v.length > 5) v = v.slice(0,5) + '-' + v.slice(5);
-      this.value = v;
+  const maskCep = (input) => {
+    input.addEventListener('input', () => {
+      let v = onlyDigits(input.value).slice(0,8);
+      if (v.length > 5) v = v.slice(0,5) + '-' + v.slice(5);
+      input.value = v;
     });
-  }
+  };
 
-  function validateCep($cep){
-    var cep = onlyDigits($cep.val());
-    if(cep.length !== 8){
-      setDisabled(true);
-      showMsg($cep, 'CEP inválido');
-      return;
-    }
-    jQuery.getJSON('https://viacep.com.br/ws/'+cep+'/json/')
-      .done(function(d){
-        if(d && d.erro){
-          setDisabled(true);
-          showMsg($cep, 'CEP inválido');
-        }else{
-          setDisabled(false);
-          showMsg($cep, '');
-        }
+  const validateCep = (input) => {
+    const cep = onlyDigits(input.value);
+    if (cep.length !== 8) { setDisabled(true); showMsg(input, 'CEP inválido'); return; }
+    fetch('https://viacep.com.br/ws/'+cep+'/json/')
+      .then(r => r.json())
+      .then(d => {
+        if (d && d.erro) { setDisabled(true); showMsg(input, 'CEP inválido'); }
+        else { setDisabled(false); showMsg(input, ''); }
       })
-      .fail(function(){
-        setDisabled(true);
-        showMsg($cep, 'CEP inválido');
-      });
-  }
+      .catch(() => { setDisabled(true); showMsg(input, 'CEP inválido'); });
+  };
 
-  // procura repetidamente até achar o campo
-  var interval = setInterval(function(){
-    var $cep = jQuery('input[name="postcode"], #postcode, #billing_postcode, #shipping_postcode');
-    if($cep.length){
-      console.log('[cepvalidator] campo encontrado');
-      clearInterval(interval);
+  // --- Busca repetida até achar um campo de CEP ---
+  const SELECTORS = [
+    'input[name="postcode"]',
+    'input[name="billing_postcode"]',
+    'input[name="shipping_postcode"]',
+    'input[id*="postcode"]',
+    'input[name*="postcode"]',
+    '#postcode', '#billing_postcode', '#shipping_postcode'
+  ].join(',');
 
-      maskCep($cep);
+  let tries = 0;
+  const timer = setInterval(() => {
+    tries++;
+    const input = $(SELECTORS);
+    if (input) {
+      clearInterval(timer);
+      maskCep(input);
       setDisabled(true);
-      validateCep($cep);
-
-      $cep.on('input blur change', function(){
-        validateCep($cep);
-      });
-    }else{
-      console.log('[cepvalidator] procurando campo...');
+      if (input.value) validateCep(input);
+      ['input','blur','change'].forEach(evt => input.addEventListener(evt, () => validateCep(input)));
+    } else if (tries > 180) {
+      clearInterval(timer); // aborta após 3 minutos
     }
   }, 1000);
-
 })();
 </script>
 HTML;
