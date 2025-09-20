80c3a27c399267d215f74dccc7aca1cb87bff13d
Otávio de Oliveira <otaviodeveloper6v@gmail.com>
2025-09-20 16:41:16 -0300

    script

--- src/Services/registerNumberteste.php
+++ src/Services/registerNumberteste.php
@@ -13,40 +13,7 @@ function registerNumberTeste () {
                 return (s || '').replace(/\\D/g, '');
             }
                 
-            function maskCpfCnpj($el){
-                console.log("[DEBUG] maskCpfCnpj() chamado para elemento:", $el);
-
-                var v = digits($el.val());
-                console.log("[DEBUG] valor digitado (apenas números):", v);
-
-                if (v.length > 14) v = v.slice(0,14);
-
-                if (v.length <= 11){
-                    if (v.length > 9)      v = v.replace(/^(\\d{3})(\\d{3})(\\d{3})(\\d{0,2}).*$/, "\$1.\$2.\$3-\$4");
-                    else if (v.length > 6) v = v.replace(/^(\\d{3})(\\d{3})(\\d{0,3}).*$/, "\$1.\$2.\$3");
-                    else if (v.length > 3) v = v.replace(/^(\\d{3})((\\d{0,3})).*$/, "\$1.\$2");
-                } else {
-                    if (v.length > 12)     v = v.replace(/^(\\d{2})(\\d{3})(\\d{3})(\\d{4})(\\d{0,2}).*$/, "\$1.\$2.\$3/\$4-\$5");
-                    else if (v.length > 8) v = v.replace(/^(\\d{2})(\\d{3})(\\d{3})(\\d{0,4}).*$/, "\$1.\$2.\$3/\$4");
-                    else if (v.length > 5) v = v.replace(/^(\\d{2})(\\d{3})(\\d{0,3}).*$/, "\$1.\$2.\$3");
-                    else if (v.length > 2) v = v.replace(/^(\\d{2})(\\d{0,3}).*$/, "\$1.\$2");
-                }
-
-                console.log("[DEBUG] valor após aplicar máscara:", v);
-
-                $el.val(v);
-                var len = digits(v).length;
-                $el.prop('maxLength', (len >= 11 ? 18 : 14));
-
-                console.log("[DEBUG] comprimento detectado:", len);
-
-                if (typeof window.__setDocLen === "function") {
-                    console.log("[DEBUG] chamando window.__setDocLen('other',", len, ")");
-                    window.__setDocLen('other', len);
-                } else {
-                    console.warn("[DEBUG] window.__setDocLen não definido!");
-                }
-            }
+       
 
             var watcher = setInterval(function(){
                 console.log("[DEBUG] watcher chamado...");
@@ -66,7 +33,6 @@ function registerNumberTeste () {
                         console.log("[DEBUG] adicionando listener para evento:", ev);
                         from.addEventListener(ev, function(){ 
                             console.log("[DEBUG] evento", ev, "detectado no campo FROM");
-                            maskCpfCnpj(from); 
                         });
                     });
                 }
