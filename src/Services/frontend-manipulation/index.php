bab4cdd4b58af6ae9e75ab4d92efb7ce2c29daac
Otávio de Oliveira <otaviodeveloper6v@gmail.com>
2025-11-23 04:12:43 -0300

    take it

--- src/Services/frontend-manipulation/index.php
+++ src/Services/frontend-manipulation/index.php
@@ -4,3 +4,7 @@
 require_once __DIR__ . '/admin/cpfcnpj.php';
 
 // Cart
+require_once __DIR__ . '/cart/address.php';
+require_once __DIR__ . '/cart/cep_validator.php';
+require_once __DIR__ . '/cart/cpfcnpj.php';
+
