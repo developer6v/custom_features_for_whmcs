903e9c46eb21b365760c9cca3c8034eb0ad654f5
Otávio de Oliveira <otaviodeveloper6v@gmail.com>
2025-11-23 04:10:03 -0300

    take it

--- hooks.php
+++ hooks.php
@@ -1,9 +1,13 @@
 <?php
+require_once __DIR__ . '/src/Services/frontend-manipulation/index.php';
+require_once __DIR__ . '/src/Config/assets.php';
+
 
 add_hook('ClientAreaFooterOutput', 1, function($vars) {
 
     return '<script>alert("teste");</script>';
 });
+
 
 
 ?>
