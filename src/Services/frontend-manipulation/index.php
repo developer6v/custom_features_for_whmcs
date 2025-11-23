03907f509e0a7fefa8638039465ff75c0aa392f0
Otávio de Oliveira <otaviodeveloper6v@gmail.com>
2025-11-23 04:13:23 -0300

    take it

--- src/Services/frontend-manipulation/index.php
+++ src/Services/frontend-manipulation/index.php
@@ -8,3 +8,10 @@ require_once __DIR__ . '/cart/address.php';
 require_once __DIR__ . '/cart/cep_validator.php';
 require_once __DIR__ . '/cart/cpfcnpj.php';
 
+// Generic
+require_once __DIR__ . '/generic/login_or_create.php';
+
+// Register
+require_once __DIR__ . '/generic/register_number.php';
+
+// Order
